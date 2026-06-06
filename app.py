417aa514f896529a6c561a706122c12110caa66a
Emmanuel17062003 <eodoom058@gmail.com>
2026-06-06 13:44:59 +0000

    align postgresql binary import structure

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 import os
-import psycopg as psycopg2  # type: ignore
+import psycopg2_binary as psycopg2 
 
 app = Flask(__name__)
 
